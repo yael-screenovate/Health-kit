9beed0bc35151ab08d75fd459131727f781637a2
Yael <yael@screenovate.com>
2019-12-11 10:12:06 +0200

    stage at 1.12

---
+++ src/components/Contact.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+// import data from './contacts.json';
+// import logo, { ReactComponent } from '../../src/';
+import './Contact.css';
+
+class Contact extends React.Component {
+  
+  sendcurrent = () => {
+   this.props.getCurrent(this.props.contact)
+   this.props.show(true)
+  }
+
+
+  
+
+  render(){
+   
+    // const {contact, getCurrent} =this.props;
+ 
+    return (
+      <div >
+            <button class="contactWrapper" onClick= {this.sendcurrent} >
+              <div style={{background: '#'+ Math.floor(Math.random()*16777215).toString(16)}} class="circle" ></div>
+              <div class="textWrapper" > 
+                <h5 class="name">{this.props.contact.name}</h5>
+                <p class="recentSms">this is the most recent sms</p>
+              </div>
+            </button>
+      </div>
+    )
+  }
+  
+   
+    
+  }
+
+
+
+
+
+ const element = < Contact/ >;
+
+export default Contact;
---
+++ src/components/Messege.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+// import data from './contacts.json';
+// import logo, { ReactComponent } from '../../src/';
+import './Messege.css';
+
+class messege extends React.Component{
+  
+  state = {
+    hidden : "",
+  }
+
+  componentWillMount () {
+    var that = this;
+  setTimeout(function() {
+  that.show();
+},
+that.props.wait);
+}
+
+show () {
+  this.setState({hidden : ""});
+}
+
+  render() { 
+    if(this.props.isfirst) {
+      return(    
+        <div  class='q-speech-bubble'>
+        <p>{this.props.messege}</p>
+       </div>
+       
+      ); 
+    }
+    if(!this.props.isfirst) {
+      return(    
+        <div class='a-speech-bubble'>
+        <p>{this.props.messege}</p>
+       </div>
+  ); 
+    }
+  
+    }
+  }
+
+
+
+const element = < messege/ >;
+
+export default messege;
