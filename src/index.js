23736ae8580d2655d0432834cc2bc78fcbb97d95
Yael Grossnass <yael@screenovate.com>
2020-07-15 21:21:58 +0300

    Health kit

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import store from './store/store'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App/>
   </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
